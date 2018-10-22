05f6cb3a9c7629c72d97ae86891d7c19e7acc711
Alexis Rico <sferadev@gmail.com>
2018-10-22 22:21:25 +0200

    sheetBuilder: Update sheet

--- src/logic/sheetBuilder.js
+++ src/logic/sheetBuilder.js
@@ -9,16 +9,38 @@ import * as utils from './utils';
  * @param builder:
  *      - element: Element to be parsed
  *      - elementMetadata: The requested metadata
+ *      - organisationUnits: Organisation Units to be imported
  * @returns {Promise<>}:
  */
 export function buildSheet(builder) {
     return new Promise(function (resolve, reject) {
         let workbook = new Excel.Workbook();
 
+        addOverviewSheet(workbook, builder.element.displayName, builder.organisationUnits);
         addDataEntrySheet(workbook, builder.element, builder.elementMetadata);
-        addMetadataSheet(workbook, builder.elementMetadata);
+        addMetadataSheet(workbook, builder.elementMetadata, builder.organisationUnits);
 
         downloadExcel(workbook, builder.element.displayName).then(() => resolve());
+    });
+}
+
+function addOverviewSheet(workbook, title, organisationUnits) {
+    let overviewSheet = workbook.addWorksheet('Overview');
+
+    // Freeze and format column titles
+    overviewSheet.row(2).freeze();
+    overviewSheet.column(1).setWidth(70);
+    overviewSheet.column(2).setWidth(30);
+    overviewSheet.column(3).setWidth(30);
+
+    // Add column titles
+    // TODO: Freeze 2 fix
+    overviewSheet.cell(1, 1, 1, 3, true).string(title).style(style);
+    overviewSheet.cell(2, 1).string('Organisation Units').style(style);
+
+    let rowId = 3;
+    _.forEach(organisationUnits, orgUnit => {
+        overviewSheet.cell(rowId++, 1).formula('_' + orgUnit.id);
     });
 }
 
@@ -26,8 +48,9 @@ export function buildSheet(builder) {
  * Add a new metadata sheet on the given workbook with the current metadata
  * @param workbook
  * @param metadata
+ * @param organisationUnits
  */
-function addMetadataSheet(workbook, metadata) {
+function addMetadataSheet(workbook, metadata, organisationUnits) {
     let metadataSheet = workbook.addWorksheet('Metadata');
 
     // Freeze and format column titles
@@ -57,6 +80,19 @@ function addMetadataSheet(workbook, metadata) {
 
         rowId++;
     });
+
+    organisationUnits.forEach(orgUnit => {
+        metadataSheet.cell(rowId, 1).string(orgUnit.id !== undefined ? orgUnit.id : '');
+        metadataSheet.cell(rowId, 2).string('organisationUnit');
+        metadataSheet.cell(rowId, 3).string(orgUnit.displayName !== undefined ? orgUnit.displayName : '');
+
+        if (orgUnit.displayName !== undefined) workbook.definedNameCollection.addDefinedName({
+            refFormula: "'Metadata'!$" + Excel.getExcelAlpha(3) + '$' + rowId,
+            name: '_' + orgUnit.id
+        });
+
+        rowId++;
+    });
 }
 
 /**
@@ -76,43 +112,31 @@ function addDataEntrySheet(workbook, element, metadata) {
     // Add column titles
     let columnId = 1;
     let groupId = 0;
+
+    // TODO: Do not hard-code OrgUnit fetching
+    createColumn(dataEntrySheet, columnId++, 'Org Unit', '=Overview!$A$3:$A$1048576');
+    createColumn(dataEntrySheet, columnId++, 'Latitude');
+    createColumn(dataEntrySheet, columnId++, 'Longitude');
+
     _.forEach(element.programStages, (programStageT) => {
         let programStage = metadata.get(programStageT.id);
-        let commonProperties = [programStage.executionDateLabel, 'Latitude', 'Longitude'];
 
-        _.forEach(commonProperties, label => {
-            dataEntrySheet.column(columnId).setWidth(20);
-            dataEntrySheet.cell(2, columnId)
-                .string(label)
-                .style(style);
-            columnId++;
-        });
+        createColumn(dataEntrySheet, columnId++, programStage.executionDateLabel);
 
-        dataEntrySheet.cell(1, columnId - commonProperties.length, 1, columnId - 1, true)
-            .string(element.displayName)
+        dataEntrySheet.cell(1, columnId - 1)
+            .string(programStage.name)
             .style(style);
 
         _.forEach(programStage.programStageSections, (programStageSectionT) => {
             let programStageSection = metadata.get(programStageSectionT.id);
             let firstColumnId = columnId;
 
-            let groupStyle = {
-                ...style,
-                fill: {
-                    type: 'pattern',
-                    patternType: 'solid',
-                    fgColor: utils.colors[groupId % utils.colors.length]
-                }
-            };
-
             _.forEach(programStageSection.dataElements, (dataElementT) => {
                 let dataElement = metadata.get(dataElementT.id);
-
-                // TODO: Add column groups
                 dataEntrySheet.column(columnId).setWidth(dataElement.formName.length / 2.5 + 10);
                 dataEntrySheet.cell(2, columnId)
                     .formula('_' + dataElement.id)
-                    .style(groupStyle);
+                    .style(groupStyle(groupId));
 
                 if (dataElement.description !== undefined) {
                     dataEntrySheet.cell(2, columnId).comment(dataElement.description, {
@@ -126,7 +150,7 @@ function addDataEntrySheet(workbook, element, metadata) {
 
             dataEntrySheet.cell(1, firstColumnId, 1, columnId - 1, true)
                 .formula('_' + programStageSection.id)
-                .style(groupStyle);
+                .style(groupStyle(groupId));
 
             groupId++;
         });
@@ -149,6 +173,31 @@ function downloadExcel(workbook, title) {
     });
 }
 
+function createColumn(sheet, columnId, label, validation = undefined) {
+    sheet.column(columnId).setWidth(20);
+    sheet.cell(2, columnId)
+        .string(label)
+        .style(style);
+    if (validation !== undefined) sheet.addDataValidation({
+        type: 'list',
+        allowBlank: true,
+        error: 'Invalid choice was chosen',
+        showDropDown: true,
+        sqref: Excel.getExcelAlpha(columnId) + '3:' + Excel.getExcelAlpha(columnId) + '1048576',
+        formulas: [validation.toString()],
+    });
+}
+
+function namedValidationToFormula(validation) {
+    let result = '=';
+    _.forEach(validation, item => {
+        result += '_' + item + ';';
+    });
+    result = result.substr(0, result.length - 1);
+    console.log(result);
+    return result;
+}
+
 /**
  * Common cell style definition
  * @type {{alignment: {horizontal: string, vertical: string, wrapText: boolean, shrinkToFit: boolean}}}
@@ -159,5 +208,21 @@ let style = {
         vertical: 'center',
         wrapText: true,
         shrinkToFit: true
+    },
+    fill: {
+        type: 'pattern',
+        patternType: 'solid',
+        fgColor: 'ffffff'
     }
 };
+
+function groupStyle(groupId) {
+    return {
+        ...style,
+        fill: {
+            type: 'pattern',
+            patternType: 'solid',
+            fgColor: utils.colors[groupId % utils.colors.length]
+        }
+    };
+}
