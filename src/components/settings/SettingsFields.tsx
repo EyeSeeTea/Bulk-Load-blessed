8c16812cc001d6e0dcb4abd41632ab61dc4cfba8
Arnau Sanchez <pyarnau@gmail.com>
2020-02-06 14:15:22 +0100

    Add setting showOrgUnitsOnGeneration

--- src/components/settings/SettingsFields.tsx
+++ src/components/settings/SettingsFields.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
-import { Typography, Checkbox, FormGroup, FormControlLabel } from "@material-ui/core";
+import { Checkbox, FormGroup, FormControlLabel } from "@material-ui/core";
 import { MultiSelector } from "d2-ui-components";
 import i18n from "../../locales";
 import Settings, { Model } from "../../logic/settings";
 import { useAppContext } from "../../contexts/api-context";
 import { Id } from "d2-api";
-import classes from "*.module.css";
 import { makeStyles } from "@material-ui/styles";
 
 export interface SettingsFieldsProps {
     settings: Settings;
     onChange: (settings: Settings) => void;
 }
+
+type BooleanField = "showOrgUnitsOnGeneration";
 
 export default function SettingsFields(props: SettingsFieldsProps) {
     const { d2 } = useAppContext();
@@ -41,6 +42,16 @@ export default function SettingsFields(props: SettingsFieldsProps) {
         [settings]
     );
 
+    function updateBoolean(field: BooleanField) {
+        return React.useCallback(
+            (ev: React.ChangeEvent<HTMLInputElement>) => {
+                const newSettings = settings.update({ [field]: ev.target.checked });
+                onChange(newSettings);
+            },
+            [settings]
+        );
+    }
+
     const modelsInfo = React.useMemo(() => {
         return settings.getModelsInfo();
     }, [settings]);
@@ -57,6 +68,20 @@ export default function SettingsFields(props: SettingsFieldsProps) {
                         label={name}
                     />
                 ))}
+            </FormGroup>
+
+            <FieldTitle>{i18n.t("Visibility")}</FieldTitle>
+
+            <FormGroup row={true}>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={settings.showOrgUnitsOnGeneration}
+                            onChange={updateBoolean("showOrgUnitsOnGeneration")}
+                        />
+                    }
+                    label={i18n.t("Show Organisation Units On Template Generation")}
+                />
             </FormGroup>
 
             <FieldTitle>{i18n.t("User groups for Template Generation")}</FieldTitle>
