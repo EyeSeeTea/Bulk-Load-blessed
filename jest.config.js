00547e26141fc22727602f9a0cc56ccba1717b99
Alexis Rico <sferadev@gmail.com>
2020-05-23 19:58:48 +0200

    Update module configuration jest

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,15 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js"],
     testPathIgnorePatterns: ["/node_modules/", "/cypress"],
     transformIgnorePatterns: ["/node_modules/(?!d2-ui-components)"],
-    modulePaths: ["src"],
+    modulePaths: ["<rootDir>"],
+    moduleDirectories: ["node_modules"],
     moduleNameMapper: {
         "raw-loader!": "<rootDir>/config/fileMock.js",
         "\\.(css|scss)$": "<rootDir>/config/styleMock.js",
         "\\.(jpg|jpeg|png|svg)$": "<rootDir>/config/fileMock.js",
     },
-    "transform": {
-        "^.+\\.[t|j]sx?$": "babel-jest"
+    transform: {
+        "^.+\\.[t|j]sx?$": "babel-jest",
     },
     testRegex: "/src/.*/.*(test|spec)\\.(jsx?|tsx?)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
