09ed67117faff4fdf26bda528a4f1a2a753c8f38
Alexis Rico <sferadev@gmail.com>
2020-05-23 19:15:18 +0200

    Remove context.require from code as it does not run on nodejs

---
+++ src/data/templates/index.ts
@@ -0,0 +1,15 @@
+import DataSetGenerated01 from "./DataSetGenerated.01";
+import DataSetGenerated02 from "./DataSetGenerated.02";
+import OldGenerated01 from "./OldGenerated.01";
+import ProgramGenerated01 from "./ProgramGenerated.01";
+import ProgramGenerated02 from "./ProgramGenerated.02";
+import ProgramGenerated03 from "./ProgramGenerated.03";
+
+export {
+    OldGenerated01,
+    DataSetGenerated01,
+    DataSetGenerated02,
+    ProgramGenerated01,
+    ProgramGenerated02,
+    ProgramGenerated03,
+};
