20faa284fd311c2df641296152fec4148efab9b6
Arnau Sanchez <pyarnau@gmail.com>
2020-08-25 12:41:05 +0200

    Fix linting

--- src/webapp/logic/sheetImport.js
+++ src/webapp/logic/sheetImport.js
@@ -7,6 +7,8 @@ import { getObjectVersion } from "./utils";
 
 // Algorithm based on:
 // https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html
+
+/*
 function checkCoordinates(coord, vs) {
     const x = coord.longitude,
         y = coord.latitude;
@@ -30,6 +32,7 @@ function checkCoordinates(coord, vs) {
 
     return false;
 }
+*/
 
 export async function getUsedOrgUnits({
     file,
@@ -141,7 +144,7 @@ export async function readSheet({
     elementMetadata,
     useBuilderOrgUnits,
     organisationUnits,
-    orgUnitCoordMap,
+    orgUnitCoordMap: _orgUnitCoordMap,
     rowOffset = 0,
     colOffset = 0,
 }) {
