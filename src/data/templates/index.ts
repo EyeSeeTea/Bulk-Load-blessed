670d874541945af7ef2f073f4202948c651e6814
Alexis Rico <sferadev@gmail.com>
2020-10-06 12:45:09 +0200

    Add module 1 configuration

--- src/data/templates/index.ts
+++ src/data/templates/index.ts
@@ -4,3 +4,4 @@ export { OldGenerated01 } from "./generated-old/OldGenerated.01";
 export { ProgramGenerated01 } from "./generated-program/ProgramGenerated.01";
 export { ProgramGenerated02 } from "./generated-program/ProgramGenerated.02";
 export { ProgramGenerated03 } from "./generated-program/ProgramGenerated.03";
+export { NHWAModule101 } from "./nhwa-module-1/NHWAModule1.01";
