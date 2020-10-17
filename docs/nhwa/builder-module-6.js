27db596b4bf4437b317777f0630ec3942f7768b2
Alexis Rico <sferadev@gmail.com>
2020-10-17 17:30:39 +0200

    Add module 6 script

--- docs/nhwa/builder-module-6.js
+++ docs/nhwa/builder-module-6.js
@@ -1,11 +1,11 @@
 // This script is meant to be executed with a window object (JS document)
 // You can run it in the Chrome Development Console and retrieve the results in JSON
 
-const rawMetadata = await (await fetch("/api/dataSets/WDyQKfAvY3V/metadata.json")).json();
+let rawMetadata = await (await fetch("/who-prod/api/dataSets/WDyQKfAvY3V/metadata.json")).json();
 
-const metadata = new Map();
+let metadata = new Map();
 
-const customRowsTab1 = [
+let customRowsTab1 = [
     { row: 8, nrOfElements: 1 },
     { row: 13, nrOfElements: 6 },
     { row: 14, nrOfElements: 6 },
@@ -16,7 +16,7 @@ const customRowsTab1 = [
     { row: 19, nrOfElements: 6 },
 ];
 
-const customRowsTab3Comments = [
+let customRowsTab3Comments = [
     { row: 9, nrOfElements: 1 },
     { row: 10, nrOfElements: 1 },
     { row: 11, nrOfElements: 1 },
@@ -28,7 +28,7 @@ const customRowsTab3Comments = [
     { row: 21, nrOfElements: 1 },
 ];
 
-const customRowsTab3YesPartialNo = [
+let customRowsTab3YesPartialNo = [
     { row: 9, nrOfElements: 3 },
     { row: 10, nrOfElements: 3 },
     { row: 11, nrOfElements: 3 },
@@ -38,118 +38,158 @@ const customRowsTab3YesPartialNo = [
     { row: 15, nrOfElements: 3 },
     { row: 21, nrOfElements: 3 },
 ];
-for (const type in rawMetadata) {
-    const elements = rawMetadata[type];
+
+for (let type in rawMetadata) {
+    let elements = rawMetadata[type];
     if (Array.isArray(elements)) elements.map(element => metadata.set(element.id, element));
 }
 
-function getDataElements(tabSelector, letters, dataRowStart, type = "input.entryfield") {
-    return Array.from(document.querySelector(tabSelector).querySelectorAll(type)).map(
+let getDataElements = ({
+    sheet,
+    orgUnitCell,
+    periodCell,
+    tabSelector,
+    letters,
+    dataRowStart,
+    type = "input.entryfield",
+}) => {
+    return Array.from(document.querySelector(tabSelector).querySelectorAll(`${type}`)).map(
         (input, i) => {
             const id = input.getAttribute("id");
-            const data = id.split("-");
+            const [dataElement, categoryOptionCombo] = id.split("-");
+
             return {
-                deuid: data[0],
-                cocuid: data[1],
-                cell_no: `${letters[i % letters.length]}${parseInt(i / letters.length) +
-                    dataRowStart}`,
-                total: input.disabled,
-                name: `${metadata.get(data[0]).name} ${metadata.get(data[1]).name}`,
+                type: "cell",
+                orgUnit: { sheet, type: "cell", ref: orgUnitCell },
+                period: { sheet, type: "cell", ref: periodCell },
+                dataElement: { type: "value", id: dataElement },
+                categoryOption: { type: "value", id: categoryOptionCombo },
+                ref: {
+                    type: "cell",
+                    sheet,
+                    ref: `${letters[i % letters.length]}${parseInt(i / letters.length) +
+                        dataRowStart}`,
+                },
             };
         }
     );
-}
+};
 
-function getDataElementsCustomRows(tabSelector, letters, rows, type = "input.entryfield") {
-    const entryfields = Array.from(document.querySelector(tabSelector).querySelectorAll(type));
+let getDataElementsCustomRows = ({
+    sheet,
+    orgUnitCell,
+    periodCell,
+    tabSelector,
+    letters,
+    rows,
+    type = "input.entryfield",
+}) => {
+    let entryfields = Array.from(document.querySelector(tabSelector).querySelectorAll(type));
     let elementCount = 0;
-    const allFields = rows.map((row, i) => {
-        const fields = [];
+    let allFields = rows.map((row, i) => {
+        let fields = [];
         for (i = 0; i < row.nrOfElements; i++) {
             const field = entryfields[elementCount + i];
             const id = field.getAttribute("id");
-            const data = id.split("-");
+            const [dataElement, categoryOptionCombo] = id.split("-");
+
             fields.push({
-                deuid: data[0],
-                cocuid: data[1],
-                cell_no: `${letters[i]}${row.row}`,
-                total: field.disabled,
-                name: `${metadata.get(data[0]).name} ${metadata.get(data[1]).name}`,
+                type: "cell",
+                orgUnit: { sheet, type: "cell", ref: orgUnitCell },
+                period: { sheet, type: "cell", ref: periodCell },
+                dataElement: { type: "value", id: dataElement },
+                categoryOption: { type: "value", id: categoryOptionCombo },
+                ref: {
+                    type: "cell",
+                    sheet,
+                    ref: `${letters[i]}${row.row}`,
+                },
             });
         }
         elementCount = elementCount + row.nrOfElements;
         return fields;
     });
     return allFields.flat();
-}
+};
 
-const dataElementsSheet1 = getDataElementsCustomRows(
-    "#tab0",
-    ["D", "E", "F", "G", "H", "I"],
-    customRowsTab1
-);
-
-const dataElementsSheet2 = [
-    ...getDataElements("#tab1", ["D", "E", "F", "G", "H"], 16),
-    ...getDataElements("#tab1", ["P", "Q", "R"], 9, "input.entrytrueonly"),
-    ...getDataElements("#tab1", ["S"], 10, "input.entryselect[value=true]"),
-    ...getDataElements("#tab1", ["E"], 9, "textarea.entryfield"),
+let dataSheet1 = [
+    ...getDataElementsCustomRows({
+        sheet: "Characteristics",
+        orgUnitCell: "V2",
+        periodCell: "I4",
+        tabSelector: "#tab0",
+        letters: ["D", "E", "F", "G", "H", "I"],
+        rows: customRowsTab1,
+    }),
 ];
 
-const dataElementsSheet3 = [
-    ...getDataElementsCustomRows(
-        "#tab2",
-        ["P", "Q", "R"],
-        customRowsTab3YesPartialNo,
-        "input.entrytrueonly"
-    ),
-    ...getDataElements("#tab2", ["O"], 20, "input.entryselect[value=true]"),
-    ...getDataElementsCustomRows("#tab2", ["E"], customRowsTab3Comments, "textarea.entryfield"),
+let dataSheet2 = [
+    ...getDataElements({
+        sheet: "Conditions",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab1",
+        letters: ["D", "E", "F", "G", "H"],
+        dataRowStart: 16,
+    }),
+    ...getDataElements({
+        sheet: "Conditions",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab1",
+        letters: ["P", "Q", "R"],
+        dataRowStart: 9,
+        type: "input.entrytrueonly",
+    }),
+    ...getDataElements({
+        sheet: "Conditions",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab1",
+        letters: ["S"],
+        dataRowStart: 10,
+        type: "input.entryselect[value=true]",
+    }),
+    ...getDataElements({
+        sheet: "Conditions",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab1",
+        letters: ["E"],
+        dataRowStart: 9,
+        type: "textarea.entryfield",
+    }),
 ];
 
-const sheet1 = {
-    sheet_type: "AGGREGATE_STATIC",
-    sheet_no: 1,
-    sheet_name: "Characteristics",
-    orgUnitIdScheme: "UID",
-    dataElementIdScheme: "UID",
-    idScheme: "UID",
-    oucode_cell: "V2",
-    year_cell: "I4",
-    last_data_column: "ZZ",
-    agg_des: dataElementsSheet1,
-};
+let dataSheet3 = [
+    ...getDataElementsCustomRows({
+        sheet: "Regulation",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab2",
+        letters: ["P", "Q", "R"],
+        rows: customRowsTab3YesPartialNo,
+        type: "input.entrytrueonly"
+    }),
+    ...getDataElements({
+        sheet: "Regulation",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab2",
+        letters: ["O"],
+        dataRowStart: 20,
+        type: "input.entryselect[value=true]"
+    }),
+    ...getDataElementsCustomRows({
+        sheet: "Regulation",
+        orgUnitCell: "N2",
+        periodCell: "I4",
+        tabSelector: "#tab2",
+        letters: ["E"],
+        rows: customRowsTab3Comments,
+        type: "textarea.entryfield"
+    }),
+];
 
-const sheet2 = {
-    sheet_type: "AGGREGATE_STATIC",
-    sheet_no: 2,
-    sheet_name: "Conditions",
-    orgUnitIdScheme: "UID",
-    dataElementIdScheme: "UID",
-    idScheme: "UID",
-    oucode_cell: "N2",
-    year_cell: "I4",
-    last_data_column: "ZZ",
-    agg_des: dataElementsSheet2,
-};
-
-const sheet3 = {
-    sheet_type: "AGGREGATE_STATIC",
-    sheet_no: 3,
-    sheet_name: "Regulation",
-    orgUnitIdScheme: "UID",
-    dataElementIdScheme: "UID",
-    idScheme: "UID",
-    oucode_cell: "N2",
-    year_cell: "I4",
-    last_data_column: "ZZ",
-    agg_des: dataElementsSheet3,
-};
-
-const module6 = {
-    name: "Module 6 Template",
-    file: "NHWA_Module_6.xlsx",
-    sheets: [sheet1, sheet2, sheet3],
-};
-
-JSON.stringify(module6, null, 4);
+let result = [...dataSheet1, ...dataSheet2, ...dataSheet3];
+console.log(result);
