e91fbfaddbe19ddd05ad44cd9cc7875ec777566e
Alexis Rico <sferadev@gmail.com>
2020-10-17 18:04:24 +0200

    Update module 8 code

--- src/data/templates/nhwa-module-8/NHWAModule8.01.ts
+++ src/data/templates/nhwa-module-8/NHWAModule8.01.ts
@@ -2,8 +2,8 @@ import { CustomTemplate, DataSource, StyleSource } from "../../../domain/entitie
 
 export class NHWAModule801 implements CustomTemplate {
     public readonly type = "custom";
-    public readonly id = "NHWA_MODULE_5_v1";
-    public readonly name = "NHWA Module 5";
+    public readonly id = "NHWA_MODULE_8_v1";
+    public readonly name = "NHWA Module 8-10";
     public readonly dataFormId = { type: "value" as const, id: "p5z7F51v1ag" };
     public readonly dataFormType = { type: "value" as const, id: "dataSets" as const };
 
