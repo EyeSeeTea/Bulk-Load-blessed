7ee75ccb8c9df854e5634a54bee5e5f668ecfb94
Alexis Rico <sferadev@gmail.com>
2020-10-22 09:37:19 +0200

    Add way to merge summaries

--- src/domain/entities/ImportSummary.ts
+++ src/domain/entities/ImportSummary.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export interface ImportSummary {
     status: "SUCCESS" | "ERROR" | "WARNING";
     stats: ImportSummaryStats;
@@ -17,4 +19,22 @@ export const emptyImportSummary: ImportSummary = {
     stats: { created: 0, updated: 0, deleted: 0, ignored: 0 },
     description: "",
     errors: [],
+};
+
+export const mergeSummaries = (...array: Array<ImportSummary | undefined>): ImportSummary => {
+    const summaries = _.compact(array);
+
+    const statusMessages = summaries.map(({ status }) => status);
+    const status = _.uniq(statusMessages).length === 1 ? statusMessages[0] : "ERROR";
+    const description = summaries.map(({ description }) => description).join("\n");
+    const errors = _.flatMap(summaries, ({ errors }) => errors);
+
+    const stats = {
+        created: _.sum(summaries.map(({ stats }) => stats.created)),
+        updated: _.sum(summaries.map(({ stats }) => stats.updated)),
+        deleted: _.sum(summaries.map(({ stats }) => stats.deleted)),
+        ignored: _.sum(summaries.map(({ stats }) => stats.ignored)),
+    };
+
+    return { status, stats, description, errors };
 };
