226b3f504d3b29c8acfb413683f1672242bf122d
Alexis Rico <sferadev@gmail.com>
2021-05-05 09:20:44 +0200

    Fix tests

--- src/test/mocks/server.ts
+++ src/test/mocks/server.ts
@@ -78,7 +78,7 @@ export function initializeMockServer() {
         params: {
             paging: false,
             fields:
-                "access,attributeValues[attribute[code],value],dataSetElements[dataElement[categoryCombo[categoryOptionCombos[id,name]],formName,id,name]],displayName,id,name,organisationUnits[id,name,path],periodType,sections[dataElements[categoryCombo[categoryOptionCombos[id,name]],formName,id,name],id,name]",
+                "access,attributeValues[attribute[code],value],dataSetElements[dataElement[categoryCombo[categoryOptionCombos[id,name]],formName,id,name]],displayName,id,name,periodType,sections[dataElements[categoryCombo[categoryOptionCombos[id,name]],formName,id,name],id,name]",
             filter: [],
         },
     }).reply(200, {
