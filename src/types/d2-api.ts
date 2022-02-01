e2a9e28989bb8e35e12aa140543eb5a777e2c495
Alexis Rico <sferadev@gmail.com>
2022-02-01 17:24:33 +0100

    Fix tests
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- src/types/d2-api.ts
+++ src/types/d2-api.ts
@@ -1,8 +1,11 @@
 import { D2Api } from "@eyeseetea/d2-api/2.33";
-import { getMockApiFromClass } from "@eyeseetea/d2-api";
 
 export * from "@eyeseetea/d2-api/2.33";
 export type { D2RelationshipConstraint } from "@eyeseetea/d2-api/schemas/base";
 
 export const D2ApiDefault = D2Api;
-export const getMockApi = getMockApiFromClass(D2Api);
+export const getMockApi = () => {
+    const api = new D2Api({ backend: "xhr" });
+    const mock = api.getMockAdapter();
+    return { api, mock };
+};
