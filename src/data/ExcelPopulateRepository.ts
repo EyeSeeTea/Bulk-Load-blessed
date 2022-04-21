7000c3fa5e34feb14cf52bae677bb0043a192430
Arnau Sanchez <pyarnau@gmail.com>
2022-04-21 10:28:42 +0200

    Use a defined name to store template version (useful on custom templates)

--- src/data/ExcelPopulateRepository.ts
+++ src/data/ExcelPopulateRepository.ts
@@ -21,13 +21,20 @@ export class ExcelPopulateRepository extends ExcelRepository {
     public async loadTemplate(options: LoadOptions): Promise<string> {
         const workbook = await this.parseFile(options);
         const forcedCleanId = options.type === "file-base64" ? options.templateId : null;
-        let cleanId: string;
         const cell = workbook.sheet(0).cell("A1");
+        const versionPrefix = "Version: ";
+
+        // We need to read and store the template ID in the spreadsheet. Keep old/new way for compatibility:
+        //  - Old way: sheets[0].cell("A1").value("Version: ID")
+        //  - New way: workbook.definedName("Version: ID")
+        let cleanId: string;
         if (forcedCleanId) {
-            cell.value(forcedCleanId);
+            workbook.definedName(versionPrefix + forcedCleanId, cell);
             cleanId = forcedCleanId;
         } else {
-            const id = cell.value();
+            const idFromDefinedName = workbook.definedName().find(name => name.startsWith(versionPrefix));
+            const idFromCell = cell.value();
+            const id = idFromDefinedName || idFromCell;
 
             if (!id || typeof id !== "string") throw new Error("Invalid id");
             cleanId = id.replace(/^.*?:/, "").trim();
