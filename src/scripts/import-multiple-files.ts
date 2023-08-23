1753e1877ec565223896af6cd093894a11777ca7
Eduardo Peredo Rivero <jrperedo@gmail.com>
2023-08-23 10:30:58 -0500

    update script to import only one file

--- src/scripts/import-multiple-files.ts
+++ src/scripts/import-multiple-files.ts
@@ -1,8 +1,8 @@
 import _ from "lodash";
 import path from "path";
 import { command, run, string, option } from "cmd-ts";
-import { resolve } from "node:path";
-import { readdir, readFile, writeFile } from "node:fs/promises";
+import { resolve, basename } from "node:path";
+import { readFile, writeFile } from "node:fs/promises";
 
 import { D2Api } from "./../types/d2-api";
 import appConfig from "../../public/app-config.json";
@@ -10,30 +10,6 @@ import { getCompositionRoot } from "../CompositionRoot";
 import { JsonConfig } from "../data/ConfigWebRepository";
 import { getD2APiFromInstance } from "../utils/d2-api";
 import Settings from "../webapp/logic/settings";
-import { promiseMap } from "../utils/promises";
-import { isExcelFile } from "../utils/files";
-
-type BufferWithFileName = {
-    content: Buffer;
-    fileName: string;
-};
-
-async function readExcelFilesInFolder(fullPathFolder: string): Promise<BufferWithFileName[]> {
-    const fileNames = await readdir(fullPathFolder);
-    console.debug(`Looking for excel files in: ${fullPathFolder}`);
-    const filesContent = await promiseMap(fileNames, async fileName => {
-        const filePath = resolve(fullPathFolder, fileName);
-        const content = isExcelFile(fileName) ? await readFile(filePath) : undefined;
-        if (!content) return undefined;
-        return {
-            content,
-            fileName,
-        };
-    });
-    const excelFiles = _(filesContent).compact().value();
-    console.debug(`${excelFiles.length} excel files found.`);
-    return excelFiles;
-}
 
 function main() {
     const cmd = command({
@@ -60,38 +36,35 @@ function main() {
             }),
         },
         handler: async args => {
-            const filesContent = await readExcelFilesInFolder(args.folderPath);
             const api: D2Api = getD2APiFromInstance({ type: "local", url: args.url });
 
-            await promiseMap(filesContent, async file => {
-                const compositionRoot = getCompositionRoot({
-                    appConfig: appConfig as unknown as JsonConfig,
-                    dhisInstance: { type: "local", url: args.url },
-                    importSource: "node",
-                });
-                const settings = await Settings.build(api, compositionRoot);
-                console.debug(`Importing file ${file.fileName}`);
-                const results = await compositionRoot.templates.import({
-                    // @ts-ignore
-                    file: file.content,
-                    settings,
-                    duplicateStrategy: "ERROR",
-                    organisationUnitStrategy: "ERROR",
-                    selectedOrgUnits: [],
-                    useBuilderOrgUnits: false,
-                });
-                const resultPath = resolve(args.resultsPath, `${file.fileName}.json`);
-                results.match({
-                    success: async syncResults => {
-                        const resultDetails = JSON.stringify(syncResults, null, 2);
-                        await writeFile(resultPath, resultDetails);
-                        console.debug(`Results saved to ${resultPath}`);
-                    },
-                    error: async errorResults => {
-                        await writeFile(resultPath, errorResults.type);
-                    },
-                });
-                return results;
+            const excelFile = await readFile(args.folderPath);
+            const compositionRoot = getCompositionRoot({
+                appConfig: appConfig as unknown as JsonConfig,
+                dhisInstance: { type: "local", url: args.url },
+                importSource: "node",
+            });
+            const settings = await Settings.build(api, compositionRoot);
+            console.debug(`Importing file ${args.folderPath}`);
+            const results = await compositionRoot.templates.import({
+                // @ts-ignore
+                file: Buffer.from(excelFile),
+                settings,
+                duplicateStrategy: "ERROR",
+                organisationUnitStrategy: "ERROR",
+                selectedOrgUnits: [],
+                useBuilderOrgUnits: false,
+            });
+            const resultPath = resolve(args.resultsPath, `${basename(args.folderPath)}.json`);
+            results.match({
+                success: async syncResults => {
+                    const resultDetails = JSON.stringify(syncResults, null, 2);
+                    await writeFile(resultPath, resultDetails);
+                    console.debug(`Results saved to ${resultPath}`);
+                },
+                error: async errorResults => {
+                    await writeFile(resultPath, errorResults.type);
+                },
             });
         },
     });
